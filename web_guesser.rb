585afc248e3853e41a6732f8f46f08d617a3bfb2
Calaway <Christopher.Calaway@gmail.com>
2016-08-08 17:04:31 -0600

    guesser is working with background colors

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,5 +1,28 @@
 require 'sinatra'
+require 'sinatra/reloader'
+
+SECRET_NUMBER = rand(101)
+
+def check_guess(guess)
+  if guess == 0
+    ["white", "Try guessing a number:"]
+  elsif guess > SECRET_NUMBER + 5
+    ["red", "Way too high!"]
+  elsif guess > SECRET_NUMBER
+    ["pink", "Too high!"]
+  elsif guess < SECRET_NUMBER - 5
+    ["red", "Way too low!"]
+  elsif guess < SECRET_NUMBER
+    ["pink", "Too low!"]
+  elsif guess == SECRET_NUMBER
+    ["green", "You got it right!<br />The secret number is #{SECRET_NUMBER}"]
+  else
+    ["white", "Try guessing a number:"]
+  end
+end
 
 get '/' do
-  "Hello, World!"
+  guess = params['guess'].to_i
+  background, message = check_guess(guess)
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :background => background}
 end
